56a7b4bad66da2e8c6a93583e4a78e353be12ae4
Bodisei <pot-bogdan@yandex.ua>
2015-09-23 21:38:23 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Kyrcu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            string text;
+
+            //Считываем данные
+            using (StreamReader sr = File.OpenText("in.txt"))
+            {
+                string s = "";
+
+                s = sr.ReadLine();
+                n = Int32.Parse(s);
+
+                text = sr.ReadLine();
+            }
+
+            using (StreamWriter sw = File.CreateText("out.txt"))
+            {
+                string[] answer = text.Split(' ');
+                int countWord = 1;
+
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (text[i] == ' ')
+                        countWord++;
+                }
+
+                for (int i = 0; i < countWord; i++)
+                {
+                    sw.WriteLine(answer[i]);
+                }
+            }
+        }
+    }
+}
