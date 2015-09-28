e9609f0e6150933584a0231a6b2438e1b98ab714
Bodisei <pot-bogdan@yandex.ua>
2015-09-28 20:01:09 +0300

    first commit

--- Program.cs
+++ Program.cs
@@ -3,26 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace SaturdayNothing
+namespace Chapter3
 {
     class Program
     {
-        private static void ConfigureCUI()
-        {
-            Console.Title = "My rocking APP";
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.BackgroundColor = ConsoleColor.Blue;
-            Console.WriteLine("********************");
-            Console.WriteLine("*Hello rocking APP!*");
-            Console.WriteLine("********************");
-            Console.BackgroundColor = ConsoleColor.Black;
-        }
-
         static void Main(string[] args)
         {
-            ConfigureCUI();
+            Console.WriteLine("Программа по преобразованию числа");
+            Console.WriteLine("Денежный формат {0:c}", 24/7);
+            Console.WriteLine("Десятичное число {0:d5}", 24/7);
+            Console.WriteLine("Число с фиксированой точкой {0:f3}", 24/7);
+            Console.WriteLine("Число с запятой {0:n}", 24/7);
+            Console.WriteLine("Число в представлянии експонента {0:e}", 24/7);
+            Console.WriteLine("Шестнадцетиричное число {0:X}", 99999);
 
             Console.ReadLine();
+
 
         }
     }
